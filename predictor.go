44e25854ced5abf94bfbc98fd9f2aea7f946e3c3
William Storey <wstorey@maxmind.com>
2023-06-08 20:00:31 +0000

    Move copyright header so it does not appear in godoc

--- predictor.go
+++ predictor.go
@@ -1,11 +1,11 @@
 // Package xgbshap calculates feature contributions for XGBoost models.
-//
+package xgbshap
+
 // Much of this code is ported from the xgboost C++ code.
 //
-// # Copyright by XGBoost Contributors 2017-2023
+// Copyright by XGBoost Contributors 2017-2023
 //
 // xgboost's code is Apache 2.0 licensed.
-package xgbshap
 
 import (
 	"fmt"
